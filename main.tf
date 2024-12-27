40207ac16c47705f390bca57941665a951ae2d74
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2024-12-27 19:25:55 +0100

    añadir validate terraform sin errores

--- main.tf
+++ main.tf
@@ -3,13 +3,13 @@
   bucket_name = var.bucket_name
 }*/
 
-modulo "s3_bucket" {
+module "s3_bucket" {
     source = "git::https://github.com/gruposdosdual/infra-resources1.git//modules/s3"
     bucket_name = var.bucket_name
 }
 
 
-modulo "s3_bucket_1" {
+module "s3_bucket_1" {
     source = "git::https://github.com/gruposdosdual/infra-resources1.git//modules/s3"
     bucket_name = var.bucket_name1
 }
