4c3fcd1fbb16bc8ba4931e8a4a45b4013bcf4138
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-09 21:05:13 +0100

    dependencias y relaciones

---
+++ practica-dependencia-relaciones/provisioners/copiar_archivo.tf
@@ -0,0 +1,90 @@
+provider "aws" {
+  region = "eu-west-3"
+  profile = "248189943700_EKS-alumnos"
+}
+
+# Crear una VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+
+# Crear un grupo de seguridad
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id  # Asegúrate de que el VPC esté definido
+
+  # Regla de entrada para permitir tráfico SSH (puerto 22)
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]  # Permitir desde cualquier IP (o usa una IP específica)
+  }
+
+  # Regla de salida para permitir todo el tráfico
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_instance" "practica_provisioner" {
+  ami           = "ami-09be70e689bddcef5" # Cambia por un AMI válido.
+  instance_type = "t2.micro"
+  key_name      = "my-key"       # Cambia por una clave SSH válida.
+  subnet_id     = aws_subnet.subnet.id  # Asegúrate de que la subred esté definida
+
+  # Usa vpc_security_group_ids en lugar de security_groups
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  # Creamos la instancia y aseguramos que tenga IP pública
+  associate_public_ip_address = true
+
+  # Espera a que la instancia esté completamente creada
+  depends_on = [aws_security_group.allow_ssh]
+
+  tags = {
+    Name = "Practica-Provisioner"
+  }
+}
+
+resource "null_resource" "provision_file" {
+  depends_on = [aws_instance.practica_provisioner]
+
+  provisioner "remote-exec" {
+    connection {
+        type        = "ssh"
+        user        = "ec2-user"     # Cambia según el AMI (puede ser 'ubuntu' o 'root').
+        private_key = file("my-key.pem") # Cambia a la ruta correcta de tu clave privada.
+        #host        = self.public_ip
+        host        = aws_instance.practica_provisioner.public_ip  # Uso de la IP pública correcta.
+  }
+
+    inline = [
+        "echo 'Configurando la instancia' > /tmp/config.cfg",
+        "sudo mv /tmp/config.cfg /home/ec2-user/config.cfg"
+    ]
+  }
+}
+/*
+
+  provisioner "file" {
+    source      = "config.cfg"
+    destination = "/tmp/config.cfg"
+  }
+*/
+ 
+
+output "instance_public_ip" {
+  value = aws_instance.practica_provisioner.public_ip
+}
