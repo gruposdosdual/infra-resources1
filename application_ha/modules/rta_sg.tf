7ba1f6a4c63c544f5afb0d4b6c1836fc19f34ae1
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-20 11:32:39 +0100

    desplegando multiregion con terragrunt 20012025-MR

--- application_ha/modules/rta_sg.tf
+++ application_ha/modules/rta_sg.tf
@@ -1,6 +1,6 @@
 # Grupo de seguridad para SSH y WORDPRESS
-resource "aws_security_group" "allow_ssh_tg5" {
-  name        = "allow_ssh_tg5"
+resource "aws_security_group" "allow_ssh_mr" {
+  name        = "allow_ssh_mr"
   description = "Allow SSH inbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
@@ -26,13 +26,13 @@ resource "aws_security_group" "allow_ssh_tg5" {
   }
 
   tags = {
-    Name = "Allow SSH-TG5"
+    Name = "Allow SSH-MR"
   }
 }
 
 # Grupo de seguridad para MySQL
-resource "aws_security_group" "allow_mysql5" {
-  name        = "allow_mysql_cinco"
+resource "aws_security_group" "allow_mysql_mr" {
+  name        = "allow_mysql_mr"
   description = "Allow MySQL inbound traffic"
   vpc_id      = data.aws_vpc.default.id
 
