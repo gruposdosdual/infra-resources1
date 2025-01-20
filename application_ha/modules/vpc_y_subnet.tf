84b3bd66db60e8e354151f2736d23f37a4f3d481
Grupo3-campusdual3 <francisco.guerra@campusdual.com>
2025-01-20 12:45:57 +0100

    despliegue multiregion con terragrunt MR-subnet nuevas

--- application_ha/modules/vpc_y_subnet.tf
+++ application_ha/modules/vpc_y_subnet.tf
@@ -1,4 +1,4 @@
-# Referenciar la VPC existente
+# Obtener la VPC predeterminada
 data "aws_vpc" "default" {
   default = true
 }
@@ -11,145 +11,61 @@ data "aws_internet_gateway" "default" {
   }
 }
 
-# Subredes predeterminadas asociadas a la VPC
-data "aws_subnet" "subnet_a" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+# Crear una subred pública en la zona de disponibilidad "eu-west-3a"
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.0.0/20" # Asegúrate de que no solape con el CIDR de otras subredes
+  availability_zone       = "eu-west-3a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet A"
   }
-
-  filter {
-    name   = "availability-zone"
-    values = ["eu-west-3a"]
-  }
-
-
-# Si conoces el CIDR de la subred, agrega este filtro para hacerlo más específico
-  #filter {
-    #name   = "cidr-block"
-    #values = ["172.31.0.0/20"]  # Reemplaza con el CIDR correcto
-  #}
 }
 
+# Crear una subred pública en la zona de disponibilidad "eu-west-3b"
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = data.aws_vpc.default.id
+  cidr_block              = "172.31.16.0/20" # Asegúrate de que no solape con el CIDR de otras subredes
+  availability_zone       = "eu-west-3b"
+  map_public_ip_on_launch = true
 
-data "aws_subnet" "subnet_b" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  tags = {
+    Name = "Public Subnet B"
   }
+}
 
-  filter {
-    name   = "availability-zone"
-    values = ["eu-west-3b"]
+# Crear una subred privada en la zona de disponibilidad "eu-west-3a"
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.32.0/20"
+  availability_zone = "eu-west-3a"
+
+  tags = {
+    Name = "Private Subnet A"
   }
+}
 
+# Crear una subred privada en la zona de disponibilidad "eu-west-3b"
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.48.0/20"
+  availability_zone = "eu-west-3b"
 
-# Si conoces el CIDR de la subred, agrega este filtro para hacerlo más específico
-  #filter {
-    #name   = "cidr-block"
-    #values = ["172.31.16.0/20"]  # Reemplaza con el CIDR correcto
-  #}
+  tags = {
+    Name = "Private Subnet B"
+  }
 }
 
 # Grupo de subredes para RDS
 resource "aws_db_subnet_group" "mysql_subnet_group" {
   name       = "mr-mysql-subnet-group"
-  subnet_ids = [data.aws_subnet.subnet_a.id, data.aws_subnet.subnet_b.id]
+  subnet_ids = [
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
 
   tags = {
-    Name = "MySQL DB subnet group"
-  }
-
-  lifecycle {
-    create_before_destroy = true    
+    Name = "MySQL DB Subnet Group"
   }
 }
-
-
-
-
-
-
-/*
-
-
-
-resource "aws_vpc" "main" {
-  cidr_block           = "172.31.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true  # Importante para RDS
-  enable_dns_support   = true  # Importante para RDS
-
-  tags = {
-    Name = "Default vpc"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "Internet Gateway-jgl"
-  }
-}
-
-# Subnets para RDS (necesitamos al menos 2 para Multi-AZ)
-resource "aws_subnet" "subnet_1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.0.0/24"
-  availability_zone       = "eu-west-3a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "Subnet-1-jgl"
-  }
-}
-
-resource "aws_subnet" "subnet_2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.31.16.0/20"
-  availability_zone       = "eu-west-3b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "Subnet-2-jgl"
-  }
-}
-
-# Grupo de subnets para RDS
-resource "aws_db_subnet_group" "mysql_subnet_group" {
-  name       = "mysql-subnet-group"
-  subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
-
-  tags = {
-    Name = "MySQL DB subnet group"
-  }
-
-  lifecycle {
-      create_before_destroy = true
-      ignore_changes = [name]
-  }
-}
-
-resource "aws_route_table" "route_table" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-  tags = {
-    Name = "Route Table-jgl"
-  }
-}
-
-resource "aws_route_table_association" "route_table_association_1" {
-  subnet_id      = aws_subnet.subnet_1.id
-  route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_route_table_association" "route_table_association_2" {
-  subnet_id      = aws_subnet.subnet_2.id
-  route_table_id = aws_route_table.route_table.id
-}
-
-*/
